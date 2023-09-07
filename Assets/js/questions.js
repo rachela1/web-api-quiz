0e076d454e6c0243be7229a83bed27749e622ef5
rachela1 <rachel_kaitlin01@hotmail.com>
2023-09-07 17:04:59 -0500

    created score.js and questions.js, moved questions from script.js to questions.js

---
+++ Assets/js/questions.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+        correct: "alerts",
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with____.",
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets",
+        correct: "curly brackets",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store_____.",
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above",
+        correct: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within____ when being assigned to variables.",
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parenthesis",
+        correct: "quotes",
+    },
+    {
+        question: "A very useful too used during development and debugging for printing content to the debugger is:",
+        a: "JavaScript",
+        b: "terminal/bash",
+        c: "for loops",
+        d: "console.log",
+        correct: "d",
+    },
+];
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,80 +1,37 @@
-var quizData = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        a: "strings",
-        b: "booleans",
-        c: "alerts",
-        d: "numbers",
-        correct: "c",
-    },
-    {
-        question: "The condition in an if / else statement is enclosed with____.",
-        a: "quotes",
-        b: "curly brackets",
-        c: "parenthesis",
-        d: "square brackets",
-        correct: "b",
-    },
-   {
-        question: "Arrays in JavaScript can be used to store_____.",
-        a: "numbers and strings",
-        b: "other arrays",
-        c: "booleans",
-        d: "all of the above",
-        correct: "d",
-   }, 
-   {
-        question: "String values must be enclosed within____ when being assigned to variables.",
-        a: "commas",
-        b: "curly brackets",
-        c: "quotes",
-        d: "parenthesis",
-        correct: "c",
-   },
-    {
-        question: "A very useful too used during development and debugging for printing content to the debugger is:",
-        a: "JavaScript",
-        b: "terminal/bash",
-        c: "for loops",
-        d: "console.log",
-        correct: "d",
-    },
-]
+var startEl = document.getElementById('#start')
+var finishEl = document.getElementById('#finish')
 
-var quiz = document.getElementById('quiz')
-var answerEls = document.querySelectorAll('.answer')
-var questionEl = document.getElementById('question')
-var aText = document.getElementById('aText')
-var bText = document.getElementById('bText')
-var cText = document.getElementById('cText')
-var dText = document.getElementById('dText')
+var quizEl = document.getElementById('#quiz')
+var answerEls = document.querySelectorAll('#answer')
+var questionsEl = document.getElementById('#questions')
+var aText = document.getElementById('#aText')
+var bText = document.getElementById('#bText')
+var cText = document.getElementById('#cText')
+var dText = document.getElementById('#dText')
+var timerEl = document.getElementById('#timer')
 
-var currentQuiz = 0
-let score = 0
 
-loadQuiz()
+function setView(view) {
+    startEl.style.display = view === "START" ? null : "none";
+    quizEl.style.display = view === "QUIZ" ? null : "none";
+    finishEl.style.display = view === "FINISH" ? null : "none";
+};
 
-function loadQuiz(){
-    deselectAnswers()
-    var currentQuizData = quizData[currentQuiz]
-    questionEl.innerText = currentQuizData.question
-    a1Text.innerText = currentQuizData.a
-    a2Text.innerText = currentQuizData.b
-    a3Text.innerText = currentQuizData.c
-    a4Text.innerText = currentQuizData.d
+function.init() {
+    timerEl.textContent = state.timeRemaining;
 }
 
-function deselectAnswers() {
-    answerEls.forEach(answerEls => answerEls.checked = false)
+function buildQuestion() {
+    var question = state.questions[questionIndex];
+    questionsEl.innerHTML = null;
+    console.log(question)
+    for {
+        (var choice of question.choices)
+}
+var buttonEl = document.createElement();
+buttonEl.textContent = choice;
 }
 
-function getSelected() {
-    var answerEls
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
-}
 
+
+init();
