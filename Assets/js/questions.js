f183f636c44b600491c57d3b73275f5d70a0d3ac
rachela1 <rachel_kaitlin01@hotmail.com>
2023-09-15 19:40:18 -0500

    fixed typos in questions.js from 'correct' to 'answer'

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -7,17 +7,17 @@ var questions = [
     {
         title: "The condition in an if / else statement is enclosed with____.",
         choices: ["quotes","curly brackets","parenthesis","square brackets"],
-        correct: "curly brackets",
+        answer: "curly brackets",
     },
     {
         title: "Arrays in JavaScript can be used to store_____.",
         choices: ["numbers and strings","other arrays","booleans","all of the above"],
-        correct: "all of the above",
+        answer: "all of the above",
     },
     {
         title: "String values must be enclosed within____ when being assigned to variables.",
         choices: ["commas","curly brackets","quotes","parenthesis"],
-        correct: "quotes",
+        answer: "quotes",
     },
     {
         title: "A very useful too used during development and debugging for printing content to the debugger is:",
